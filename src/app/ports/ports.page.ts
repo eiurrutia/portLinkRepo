46b11d46c02de894fcf3e1db453b4cdbb2b6119e
Enrique <eiurrutia@uc.cl>
2019-04-18 12:51:32 -0400

    ports section buttons design added

--- src/app/ports/ports.page.ts
+++ src/app/ports/ports.page.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ports.page.scss'],
 })
 export class PortsPage implements OnInit {
-
+  public selected_option = '1';
+  public text: any;
+  // 1 --> "Puertos Actuales"
+  // 2 --> "Nuevo Puerto"
+  // 3 --> "Historial"
+  // 4 --> "Estadísticas"
   constructor() { }
 
   ngOnInit() {
   }
+
+  selectOption() {
+    console.log('click on: ');
+  }
+
 
 }
