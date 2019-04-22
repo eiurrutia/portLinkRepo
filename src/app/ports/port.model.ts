88428e6fe5f42828b31052b469455354b8e4e471
Enrique <eiurrutia@uc.cl>
2019-04-22 17:54:36 -0400

    ports model added

---
+++ src/app/ports/port.model.ts
@@ -0,0 +1,31 @@
+export class Port {
+    id: number;
+    shipName: string;
+    initalDate: any; // Date
+    unitsToCollect: number;
+    collectedUnits: number; // Date
+    totalDriversConsidered: number;
+    thirdDriversConsidered: number;
+    ownDriversConsidered: number;
+    turnsEstimated: number;
+    turnsMade: number;
+    extraBigUnitsQuantity: number;
+    bigUnitsQuantity: number;
+    mediumUnitsQuantity: number;
+    littleUnitsQuantity: number;
+    port: string;
+    destination: string;
+    observation: string;
+    expectedBilling: number;
+    state: string;
+    contactFirstName: string;
+    contactLastName: string;
+    contactEmail: string;
+    contactCellphone: string;
+    // Stats
+    timeNeeded: number; // Number of days
+    totalTurns: number;
+    averageOfUnitsPerTurn: number;
+    averageTimePerTurn: number;
+    damages: boolean;
+}
