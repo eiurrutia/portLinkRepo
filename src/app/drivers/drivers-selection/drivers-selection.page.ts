abd66c0b7a92ee843574fc86e2f633cc25b5839e
Enrique <eiurrutia@uc.cl>
2019-05-10 17:04:56 -0400

    drivers and thirds slide added with segment

--- src/app/drivers/drivers-selection/drivers-selection.page.ts
+++ src/app/drivers/drivers-selection/drivers-selection.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-drivers-selection',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriversSelectionPage implements OnInit {
 
+   @ViewChild(IonSlides) slides: IonSlides;
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 100
+  };
+  activeDriversDicc = {'Manuel Sáez': true,
+                       'Juan Marchant': false,
+                       'Robin Pinilla': true,
+                       'Jorge Contreras': false,
+                       'Gastón Cataldo': false,
+                       'Victor Moreno': true,
+                       'Jorge Osorio': true,
+                       'Luis González': true,
+                       'Cristian Espinoza': false,
+                       'Moises Sepulveda': false,
+                       'Miguel Vargas': false,
+                       'Francisco Bravo': true,
+                       'Miguel Bravo': false};
+
+  activeDriversDiccKeys: any;
+  segment = 'drivers';
+  firstMove = true;
+
   constructor() { }
 
   ngOnInit() {
+    this.activeDriversDiccKeys = Object.keys(this.activeDriversDicc);
+  }
+
+  segmentChanged(ev: any) {
+    if (this.firstMove) {
+      this.firstMove = false;
+      if (ev.detail.value === 'thirds') {
+        this.slides.slideNext();
+      } else {this.slides.slidePrev(); }
+    } else {this.firstMove = true; }
+
+  }
+
+  slideChanged(ev: any) {
+    if (this.firstMove) {
+      this.firstMove = false;
+      this.changeSegment();
+    } else {this.firstMove = true; }
+  }
+
+  changeSegment() {
+    if (this.segment === 'drivers') {
+      this.segment = 'thirds';
+    } else { this.segment = 'drivers'; }
   }
 
 }
