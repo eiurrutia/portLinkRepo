8165926fcc8ef3db1ce8cd9d50b2687b647d71df
Enrique <eiurrutia@uc.cl>
2019-05-15 15:44:17 -0400

    ports action created

---
+++ src/app/ports/ports-action/ports-action.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ports-action',
+  templateUrl: './ports-action.page.html',
+  styleUrls: ['./ports-action.page.scss'],
+})
+export class PortsActionPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
