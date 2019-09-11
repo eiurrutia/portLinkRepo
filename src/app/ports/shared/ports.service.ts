c62d035eb809303bb78a6db875bd2d800bb54b45
Enrique <eiurrutia@uc.cl>
2019-09-11 13:10:27 -0300

    create port function added

--- src/app/ports/shared/ports.service.ts
+++ src/app/ports/shared/ports.service.ts
@@ -26,8 +26,13 @@ export class PortsService {
     return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
   }
 
+  createPort(port: any): Observable<any> {
+    const url = `${this.backUrl}/ports/`;
+    return this.http.post<any>(url, port, this.httpOptions).catch(this.errorHandler);
+  }
+
   errorHandler(error: HttpErrorResponse) {
-        return Observable.throwError(error.status  || 'Server Error');
+    return Observable.throwError(error.status  || 'Server Error');
   }
 
 }
