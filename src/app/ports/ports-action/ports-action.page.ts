e83271d272ef43617a2a7fa36c389d4629043536
Enrique <eiurrutia@uc.cl>
2019-09-24 12:00:26 -0300

    get units by port added to packing view

--- src/app/ports/ports-action/ports-action.page.ts
+++ src/app/ports/ports-action/ports-action.page.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+
+import { PortsService } from '../shared/ports.service';
+import { UnitsService } from '../../units/shared/units.service';
+
 import { Port } from '../port.model';
+import { Unit } from '../../units/unit.model';
 
 @Component({
   selector: 'app-ports-action',
@@ -11,6 +16,8 @@ import { Port } from '../port.model';
 export class PortsActionPage implements OnInit {
   portsArray: any;
   portId = null;
+  currentPort: any;
+  unitsList: any;
 
   correctSlectedDriver = false;
   lastSelectedDriver: string;
@@ -448,88 +455,26 @@ export class PortsActionPage implements OnInit {
   wordSearchedInPacking: string;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private alertController: AlertController) { }
+              private alertController: AlertController,
+              private portsService: PortsService,
+              private unitsService: UnitsService) { }
 
   ngOnInit() {
     this.driversFiltered = this.activeDriversDicc;
     this.portId = this.activatedRoute.snapshot.paramMap.get('id');
-    console.log(this.portId);
+    this.getPort(this.portId);
 
-    this.packingListHeaders = Object.keys(this.packingListExample[0]);
+
+  }
+
+  buildPackingListHeaders() {
+    this.packingListHeaders = Object.keys(this.unitsList[0]);
     const index = this.packingListHeaders.indexOf('vin', 0);
     if (index > -1) {
       this.packingListHeaders.splice(index, 1);
     }
     console.log(this.packingListHeaders);
-
-    this.portsArray = {};
-    this.portsArray[0] = new Port();
-    this.portsArray[0].shipName = 'Morning Peace';
-    this.portsArray[0].unitsToCollect = 285;
-    this.portsArray[0].collectedUnits = 20;
-    this.portsArray[0].modelsQuantityInPacking = 305;
-    this.portsArray[0].turnsEstimated = 25;
-    this.portsArray[0].turnsMade = 10;
-    this.portsArray[0].extraBigUnitsQuantity = 20;
-    this.portsArray[0].bigUnitsQuantity = 15;
-    this.portsArray[0].mediumUnitsQuantity = 20;
-    this.portsArray[0].littleUnitsQuantity = 5;
-    this.portsArray[0].collectedExtraBigUnits = 2;
-    this.portsArray[0].collectedBigUnits = 10;
-    this.portsArray[0].collectedMediumUnits = 9;
-    this.portsArray[0].collectedLittleUnits = 3;
-    this.portsArray[0].toCollectExtraBigUnits = 18;
-    this.portsArray[0].toCollectBigUnits = 5;
-    this.portsArray[0].toCollectMediumUnits = 11;
-    this.portsArray[0].toCollectLittleUnits = 2;
-    this.portsArray[0].damages = 5;
-    this.portsArray[0].advancedPercentage = this.portsArray[0].collectedUnits / this.portsArray[0].modelsQuantityInPacking;
-
-    this.portsArray[1] = new Port();
-    this.portsArray[1].shipName = 'Cecilie';
-    this.portsArray[1].unitsToCollect = 85;
-    this.portsArray[1].collectedUnits = 30;
-    this.portsArray[1].modelsQuantityInPacking = 115;
-    this.portsArray[1].turnsEstimated = 25;
-    this.portsArray[1].turnsMade = 10;
-    this.portsArray[1].extraBigUnitsQuantity = 0;
-    this.portsArray[1].bigUnitsQuantity = 0;
-    this.portsArray[1].mediumUnitsQuantity = 20;
-    this.portsArray[1].littleUnitsQuantity = 5;
-    this.portsArray[1].collectedExtraBigUnits = 2;
-    this.portsArray[1].collectedBigUnits = 10;
-    this.portsArray[1].collectedMediumUnits = 9;
-    this.portsArray[1].collectedLittleUnits = 3;
-    this.portsArray[1].toCollectExtraBigUnits = 18;
-    this.portsArray[1].toCollectBigUnits = 5;
-    this.portsArray[1].toCollectMediumUnits = 11;
-    this.portsArray[1].toCollectLittleUnits = 2;
-    this.portsArray[1].damages = 2;
-    this.portsArray[1].advancedPercentage = this.portsArray[1].collectedUnits / this.portsArray[1].modelsQuantityInPacking;
-
-    this.portsArray[2] = new Port();
-    this.portsArray[2].shipName = 'Pleaiades Leader';
-    this.portsArray[2].unitsToCollect = 20;
-    this.portsArray[2].collectedUnits = 100;
-    this.portsArray[2].modelsQuantityInPacking = 120;
-    this.portsArray[2].turnsEstimated = 16;
-    this.portsArray[2].turnsMade = 3;
-    this.portsArray[2].extraBigUnitsQuantity = 0;
-    this.portsArray[2].bigUnitsQuantity = 0;
-    this.portsArray[2].mediumUnitsQuantity = 20;
-    this.portsArray[2].littleUnitsQuantity = 0;
-    this.portsArray[2].collectedExtraBigUnits = 2;
-    this.portsArray[2].collectedBigUnits = 10;
-    this.portsArray[2].collectedMediumUnits = 9;
-    this.portsArray[2].collectedLittleUnits = 3;
-    this.portsArray[2].toCollectExtraBigUnits = 18;
-    this.portsArray[2].toCollectBigUnits = 5;
-    this.portsArray[2].toCollectMediumUnits = 11;
-    this.portsArray[2].toCollectLittleUnits = 2;
-    this.portsArray[2].damages = 3;
-    this.portsArray[2].advancedPercentage = this.portsArray[2].collectedUnits / this.portsArray[2].modelsQuantityInPacking;
   }
-
 
   selectItem(driver: string) {
     this.selectedDriver = driver;
@@ -591,6 +536,36 @@ export class PortsActionPage implements OnInit {
     console.log(driverArray);
     console.log(sizeArray);
   }
+
+
+  getPort(portId: string): void {
+    this.portsService.getPort(portId).subscribe(
+      currentPort => {
+        this.currentPort = currentPort;
+        console.log('este es el current port');
+        console.log(currentPort);
+        this.getCurrentPortUnits(currentPort._id);
+      },
+      error => {
+        console.log(`Error fetching port`);
+      }
+    );
+  }
+
+  getCurrentPortUnits(portId: string): void {
+    this.unitsService.getUnitsByPort(portId).subscribe(
+      unitsList => {
+        this.unitsList = unitsList.data;
+        this.buildPackingListHeaders();
+        console.log('este es el units list');
+        console.log(this.unitsList);
+      },
+      error => {
+        console.log(`Error fetching units by port`);
+      }
+    );
+  }
+
 
 
 }
