2d7202be4979f8235d24b983ee8842ce7b6ee788
Enrique <eiurrutia@uc.cl>
2019-09-24 13:41:13 -0300

    drivers service created

---
+++ src/app/drivers/shared/drivers.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+import { Globals } from '../../globals';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DriversService {
+
+  constructor() { }
+}
