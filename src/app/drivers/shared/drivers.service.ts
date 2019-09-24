dfe3732a1b7ba43f138b56ca945a0d2e020344db
Enrique <eiurrutia@uc.cl>
2019-09-24 13:54:02 -0300

    function to drivers service added

--- src/app/drivers/shared/drivers.service.ts
+++ src/app/drivers/shared/drivers.service.ts
@@ -12,5 +12,30 @@ import { Globals } from '../../globals';
 })
 export class DriversService {
 
-  constructor() { }
+  httpOptions = {headers: new HttpHeaders({'Content-Type': 'application/json'})};
+  backUrl = this.globals.url;
+
+  constructor(private http: HttpClient,
+              private globals: Globals) { }
+
+
+  getDrivers(): Observable<any> {
+    const url = `${this.backUrl}/drivers/`;
+    return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  getDriver(id: string): Observable<any> {
+    const url = `${this.backUrl}/drivers/${id}/`;
+    return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  createDriver(driver: any): Observable<any> {
+    const url = `${this.backUrl}/drivers/`;
+    return this.http.post<any>(url, driver, this.httpOptions).catch(this.errorHandler);
+  }
+
+  errorHandler(error: HttpErrorResponse) {
+    return Observable.throwError(error.status  || 'Server Error');
+  }
+
 }
