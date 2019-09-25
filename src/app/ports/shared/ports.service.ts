46736e1e468b1648b65e739916f9f1b9782ce833
Enrique <eiurrutia@uc.cl>
2019-09-25 11:19:35 -0300

    patch port with considered drivers and thirds added

--- src/app/ports/shared/ports.service.ts
+++ src/app/ports/shared/ports.service.ts
@@ -36,6 +36,11 @@ export class PortsService {
     return this.http.post<any>(url, port, this.httpOptions).catch(this.errorHandler);
   }
 
+  associateDriversToPort(id: string, port: any): Observable<any> {
+    const url = `${this.backUrl}/ports/${id}/`;
+    return this.http.patch<any>(url, port, this.httpOptions).catch(this.errorHandler);
+  }
+
   errorHandler(error: HttpErrorResponse) {
     return Observable.throwError(error.status  || 'Server Error');
   }
