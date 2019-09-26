d6df9b1c3d811450b54e3a0141b703945bdb6351
Enrique <eiurrutia@uc.cl>
2019-09-26 12:41:06 -0300

    active and recents ports arrays implemented

--- src/app/ports/ports.page.ts
+++ src/app/ports/ports.page.ts
@@ -14,17 +14,11 @@ export class PortsPage implements OnInit {
 
   public selected_option = '1';
   public text: any;
-  activeItemsList: boolean;
-  recentItemsList: boolean;
-  // 1 --> "Puertos Actuales"
-  // 2 --> "Nuevo Puerto"
-  // 3 --> "Historial"
-  // 4 --> "Estadísticas"
+  activeItemsList = true;
+  recentItemsList = false;
 
-  // to try
-  pleiadesDetail: boolean;
-  portsArray: Port[];
-  portsList: Port[];
+  activePortsList: Port[];
+  recentPortsList: Port[];
 
   @Output() sendCount: EventEmitter <boolean> = new EventEmitter<boolean>();
 
@@ -36,83 +30,8 @@ export class PortsPage implements OnInit {
               private portsService: PortsService) { }
 
   ngOnInit() {
-
-    this.getPorts();
-
-    this.activeItemsList = true;
-    this.recentItemsList = false;
-
-    this.pleiadesDetail = false;
-
-    this.portsArray = [];
-    this.portsArray.push(new Port());
-    this.portsArray[0].id = 0;
-    this.portsArray[0].shipName = 'Morning Peace';
-    this.portsArray[0].unitsToCollect = 285;
-    this.portsArray[0].collectedUnits = 20;
-    this.portsArray[0].modelsQuantityInPacking = 305;
-    this.portsArray[0].turnsEstimated = 25;
-    this.portsArray[0].turnsMade = 10;
-    this.portsArray[0].extraBigUnitsQuantity = 20;
-    this.portsArray[0].bigUnitsQuantity = 15;
-    this.portsArray[0].mediumUnitsQuantity = 20;
-    this.portsArray[0].littleUnitsQuantity = 5;
-    this.portsArray[0].collectedExtraBigUnits = 2;
-    this.portsArray[0].collectedBigUnits = 10;
-    this.portsArray[0].collectedMediumUnits = 9;
-    this.portsArray[0].collectedLittleUnits = 3;
-    this.portsArray[0].toCollectExtraBigUnits = 18;
-    this.portsArray[0].toCollectBigUnits = 5;
-    this.portsArray[0].toCollectMediumUnits = 11;
-    this.portsArray[0].toCollectLittleUnits = 2;
-    this.portsArray[0].damages = 5;
-    this.portsArray[0].advancedPercentage = this.portsArray[0].collectedUnits / this.portsArray[0].modelsQuantityInPacking;
-
-    this.portsArray.push(new Port());
-    this.portsArray[1].id = 1;
-    this.portsArray[1].shipName = 'Cecilie';
-    this.portsArray[1].unitsToCollect = 85;
-    this.portsArray[1].collectedUnits = 30;
-    this.portsArray[1].modelsQuantityInPacking = 115;
-    this.portsArray[1].turnsEstimated = 25;
-    this.portsArray[1].turnsMade = 10;
-    this.portsArray[1].extraBigUnitsQuantity = 0;
-    this.portsArray[1].bigUnitsQuantity = 0;
-    this.portsArray[1].mediumUnitsQuantity = 20;
-    this.portsArray[1].littleUnitsQuantity = 5;
-    this.portsArray[1].collectedExtraBigUnits = 2;
-    this.portsArray[1].collectedBigUnits = 10;
-    this.portsArray[1].collectedMediumUnits = 9;
-    this.portsArray[1].collectedLittleUnits = 3;
-    this.portsArray[1].toCollectExtraBigUnits = 18;
-    this.portsArray[1].toCollectBigUnits = 5;
-    this.portsArray[1].toCollectMediumUnits = 11;
-    this.portsArray[1].toCollectLittleUnits = 2;
-    this.portsArray[1].damages = 2;
-    this.portsArray[1].advancedPercentage = this.portsArray[1].collectedUnits / this.portsArray[1].modelsQuantityInPacking;
-
-    this.portsArray.push(new Port());
-    this.portsArray[2].id = 2;
-    this.portsArray[2].shipName = 'Pleaiades Leader';
-    this.portsArray[2].unitsToCollect = 20;
-    this.portsArray[2].collectedUnits = 100;
-    this.portsArray[2].modelsQuantityInPacking = 120;
-    this.portsArray[2].turnsEstimated = 16;
-    this.portsArray[2].turnsMade = 3;
-    this.portsArray[2].extraBigUnitsQuantity = 0;
-    this.portsArray[2].bigUnitsQuantity = 0;
-    this.portsArray[2].mediumUnitsQuantity = 20;
-    this.portsArray[2].littleUnitsQuantity = 0;
-    this.portsArray[2].collectedExtraBigUnits = 2;
-    this.portsArray[2].collectedBigUnits = 10;
-    this.portsArray[2].collectedMediumUnits = 9;
-    this.portsArray[2].collectedLittleUnits = 3;
-    this.portsArray[2].toCollectExtraBigUnits = 18;
-    this.portsArray[2].toCollectBigUnits = 5;
-    this.portsArray[2].toCollectMediumUnits = 11;
-    this.portsArray[2].toCollectLittleUnits = 2;
-    this.portsArray[2].damages = 3;
-    this.portsArray[2].advancedPercentage = this.portsArray[2].collectedUnits / this.portsArray[2].modelsQuantityInPacking;
+    this.getActivePorts();
+    this.getRecentPorts();
 
   }
 
@@ -128,18 +47,26 @@ export class PortsPage implements OnInit {
     this.recentItemsList = !this.recentItemsList;
   }
 
-  changePleiadesDetail() {
-    this.pleiadesDetail = !this.pleiadesDetail;
-  }
-
-  getPorts(): void {
-    this.portsService.getPorts().subscribe(
+  getActivePorts(): void {
+    this.portsService.getActivePorts().subscribe(
       portsList => {
-        this.portsList = portsList.data;
-        console.log(this.portsList);
+        this.activePortsList = portsList.data;
+        console.log(this.activePortsList);
       },
       error => {
-        console.log(`Error fetching ports`);
+        console.log(`Error fetching active ports`);
+      }
+    );
+  }
+
+  getRecentPorts(): void {
+    this.portsService.getRecentPorts().subscribe(
+      portsList => {
+        this.recentPortsList = portsList.data;
+        console.log(this.recentPortsList);
+      },
+      error => {
+        console.log(`Error fetching recent ports`);
       }
     );
   }
