f80ade2fe96985a11041f48516e9ddab1eaef82f
Enrique <eiurrutia@uc.cl>
2019-10-01 14:03:29 -0300

    get laps by driver order by relativeNumber added

--- src/app/laps/shared/laps.service.ts
+++ src/app/laps/shared/laps.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+import { Globals } from '../../globals';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LapsService {
 
-  constructor() { }
+  httpOptions = {headers: new HttpHeaders({'Content-Type': 'application/json'})};
+  backUrl = this.globals.url;
+
+  constructor(private http: HttpClient,
+              private globals: Globals)  { }
+
+
+  getLaps(): Observable<any> {
+    const url = `${this.backUrl}/laps/`;
+    return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  getLapsByPort(portId: string): Observable<any> {
+    const url = `${this.backUrl}/laps?port=${portId}`;
+    return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  getLap(id: string): Observable<any> {
+    const url = `${this.backUrl}/laps/${id}/`;
+    return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  createLap(lap: any): Observable<any> {
+    const url = `${this.backUrl}/laps/`;
+    return this.http.post<any>(url, lap, this.httpOptions).catch(this.errorHandler);
+  }
+
+  getLapsByDriverAndPort(driverId: string, portId: string): Observable<any> {
+    const url = `${this.backUrl}/laps?driver=${driverId}&port=${portId}/`;
+    return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  getLapsByDriverAndPortOrderByRelativeNumber(driverId: string, portId: string): Observable<any> {
+    const url = `${this.backUrl}/laps?driver=${driverId}&port=${portId}&$sort[relativeNumber]=-1/`;
+    return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  updateLapLoad(loadId: string, lap: string): Observable<any> {
+    const url = `${this.backUrl}/laps/${loadId}/`;
+    return this.http.patch<any>(url, lap, this.httpOptions).catch(this.errorHandler);
+  }
+
+  deleteLap(portId: string): Observable<any> {
+    const url = `${this.backUrl}/laps/${portId}`;
+    return this.http.delete<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  errorHandler(error: HttpErrorResponse) {
+    return Observable.throwError(error.status  || 'Server Error');
+  }
+
+
 }
