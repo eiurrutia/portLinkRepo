96d11ee95dd00a28a3c2ef22687e45a2abe73c7e
Enrique <eiurrutia@uc.cl>
2019-10-23 12:40:07 -0300

    trucks services functions implemented

--- src/app/trucks/shared/trucks.service.ts
+++ src/app/trucks/shared/trucks.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+import { Globals } from '../../globals';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrucksService {
 
-  constructor() { }
+  httpOptions = {headers: new HttpHeaders({'Content-Type': 'application/json'})};
+  backUrl = this.globals.url;
+
+  constructor(private http: HttpClient,
+              private globals: Globals) { }
+
+
+  getTrucks(): Observable<any> {
+    const url = `${this.backUrl}/trucks/`;
+    return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  getTruck(id: string): Observable<any> {
+    const url = `${this.backUrl}/trucks/${id}/`;
+    return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  getTruckByPlateNumber(plateNumber: string): Observable<any> {
+    const url = `${this.backUrl}/trucks?plateNumber=${plateNumber}`;
+    return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  createTruck(truck: any): Observable<any> {
+    const url = `${this.backUrl}/trucks/`;
+    return this.http.post<any>(url, truck, this.httpOptions).catch(this.errorHandler);
+  }
+
+  errorHandler(error: HttpErrorResponse) {
+    return Observable.throwError(error.status  || 'Server Error');
+  }
 }
