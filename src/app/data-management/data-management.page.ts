0575549eeeca91f8578e834a35073e01888f9268
Enrique <eiurrutia@uc.cl>
2019-11-06 14:47:50 -0300

    data management page created

---
+++ src/app/data-management/data-management.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-management',
+  templateUrl: './data-management.page.html',
+  styleUrls: ['./data-management.page.scss'],
+})
+export class DataManagementPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
