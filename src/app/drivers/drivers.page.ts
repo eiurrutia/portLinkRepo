d96fb59ecda5e25f0ad0750bd007345c9c2d33e2
Enrique <eiurrutia@uc.cl>
2019-11-07 14:32:10 -0300

    compare function to sort dirvers list added

--- src/app/drivers/drivers.page.ts
+++ src/app/drivers/drivers.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DriversService } from './shared/drivers.service';
+
+
 @Component({
   selector: 'app-drivers',
   templateUrl: './drivers.page.html',
@@ -9,17 +12,57 @@ export class DriversPage implements OnInit {
 
   // To know what is the setted option: 'main', 'new', 'edit', 'delelte' or 'list'.
   settedOption = 'main';
+  drivers: any;
 
-  constructor() { }
+  constructor(private driversService: DriversService) { }
 
   ngOnInit() {
-  
+    this.getDivers();
   }
 
 
+  // To set value to view control variable.
   changeViewOption(option: string) {
     this.settedOption = option;
     console.log(this.settedOption);
   }
+
+
+  // Get drivers from backend.
+  getDivers() {
+    this.driversService.getDrivers().subscribe(
+      driversList => {
+        this.drivers = driversList.sort( this.compareLastNames );
+      },
+      error => {
+        console.log('Error fetching drivers from data-management: ', error);
+      }
+    );
+  }
+
+
+  // To sort by last name
+  // Compare function to order drivers lists
+  compareLastNames(driver1, driver2) {
+    const lastName1 = driver1.name.split(' ')[1];
+    const lastName2 = driver2.name.split(' ')[1];
+    const secondLastName1 = driver1.name.split(' ')[2];
+    const secondLastName2 = driver2.name.split(' ')[2];
+    
+    if (lastName1 > lastName2) {
+      return 1;
+    } else if (lastName1 < lastName2) {
+      return -1;
+    } else {
+      if (secondLastName1 > secondLastName2) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  }
+
+
+
 
 }
