038cb2b4f82898410ae46583dec0d1f6cec14489
Enrique <eiurrutia@uc.cl>
2019-11-08 12:56:26 -0300

    rut verificator implemented

--- src/app/drivers/drivers.page.ts
+++ src/app/drivers/drivers.page.ts
@@ -20,6 +20,7 @@ export class DriversPage implements OnInit {
 
   ngOnInit() {
     this.getDivers();
+    this.checkRut('18.855833-k');
   }
 
 
@@ -83,8 +84,59 @@ export class DriversPage implements OnInit {
   convertNamesAndLastnames() {
     this.diccNewDriverForm['name'] = this.convertNames(this.diccNewDriverForm['name']);
     this.diccNewDriverForm['fatherLastName'] = this.convertNames(this.diccNewDriverForm['fatherLastName']);
-    this.diccNewDriverForm['motherLastName'] = this.convertNames(this.diccNewDriverForm['motherLastName']);  
+    this.diccNewDriverForm['motherLastName'] = this.convertNames(this.diccNewDriverForm['motherLastName']);
   }
+
+
+  // Check Driver rut
+  checkRut(rut: string): boolean {
+    // Remove the dots
+    let value = rut.replace(/\./g, '');
+    // Remove the hyphen
+    value = value.replace(/\-/g, '');
+
+    // Divide by body and check digit.
+    const body = value.slice(0, -1);
+    const digit = value.slice(-1).toUpperCase();
+
+    // Rut format
+    rut = body + '-' + digit;
+
+    // Check min length of body ej. (n.nnn.nnn)
+    if (body.length < 7) { console.log('RUT Incompleto por extensión'); return false; }
+
+    // Check correct digit.
+    let sum = 0;
+    let multiple = 2;
+
+    // For each digit of body
+    for (let i = 1; i <= body.length; i++) {
+
+        // Get product with the corresponding multiple.
+        const index = multiple * parseInt(value[body.length - i], 0);
+
+        // Sum to the general count
+        sum = sum + index;
+
+        // Multiple inside the range [2,7]
+        if (multiple < 7) { multiple = multiple + 1; } else { multiple = 2; }
+    }
+
+    // Calculate the digit with mod 11
+    const dvExpected = 11 - (sum % 11);
+
+    // Specials cases (0 y K)
+    let readedDigit;
+    readedDigit = (digit === 'K') ? 10 : ((digit === '0' ) ? 11 : parseInt(digit, 0));
+
+
+    // Check if digit is correct
+    if (dvExpected !== readedDigit) { console.log('RUT Inválido por dígito'); return false; }
+
+    // Is valid!
+    console.log('is valid!');
+    return true;
+}
 
 
 
