48e4a1f13292dd1fa6174651031d0cba1dd4590e
Enrique <eiurrutia@uc.cl>
2019-11-13 11:06:59 -0300

    edit driver implemented

--- src/app/drivers/drivers.page.ts
+++ src/app/drivers/drivers.page.ts
@@ -16,6 +16,9 @@ export class DriversPage implements OnInit {
   drivers: any;
 
   diccNewDriverForm = {};
+  diccModifyDriverForm = {};
+  driverToModify: any;
+  selectDriverToModifyDisable = false;
 
   loading: any;
 
@@ -88,6 +91,14 @@ export class DriversPage implements OnInit {
   validateNamesAndLastNames(toValidate: string): boolean {
     console.log(this.diccNewDriverForm[toValidate].split(' '));
     if (this.diccNewDriverForm[toValidate].split(' ').length > 1) { return false;
+    } else { return true; }
+  }
+
+
+  // Validate Names and Lastnames To Modify
+  validateNamesAndLastNamesToModify(toValidate: string): boolean {
+    console.log(this.diccModifyDriverForm[toValidate].split(' '));
+    if (this.diccModifyDriverForm[toValidate].split(' ').length > 1) { return false;
     } else { return true; }
   }
 
@@ -257,10 +268,12 @@ export class DriversPage implements OnInit {
       () => {
         console.log('Conductor creado exitosamente');
         this.clearForm();
+        this.succesfulRegistrationAlert();
       },
       (error: any) => {
         console.log('Error al crear el conductor: ', error);
         if (error['status'] === 409) { this.repeatedDriverAlert(); }
+        this.failedRegistrationAlert();
       }
     );
   }
@@ -294,7 +307,169 @@ export class DriversPage implements OnInit {
   }
 
 
+  // Modify driver alert.
+  async modifyDriverAlert(driver: any) {
+    const alert = await this.alertController.create({
+      header: 'Modificar Conductor',
+      subHeader: `Deseas modificar el siguiente conductor:`,
+      message: `·Nombre: ${driver['name']}<br>
+               ·Rut: ${driver['rut']}<br>
+                `,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            this.selectDriverToModifyDisable = false;
+            this.driverToModify = null;
+          }
+        }, {
+          text: 'Aceptar',
+          handler: () => {
+            this.setDriverInfoToModifyFields();
+            this.selectDriverToModifyDisable = true;
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 
 
+  // Set driver to modify to edit fields.
+  setDriverInfoToModifyFields() {
+    const nameSplited = this.driverToModify['name'].split(' ');
+    this.diccModifyDriverForm['name'] = nameSplited[0];
+    this.diccModifyDriverForm['fatherLastName'] = nameSplited[1];
+    this.diccModifyDriverForm['motherLastName'] = nameSplited[2];
+    this.diccModifyDriverForm['rut'] = this.driverToModify['rut'];
+    this.diccModifyDriverForm['unionized'] = this.driverToModify['unionized'] ? 'Sí' : 'No';
+  }
+
+
+  // Check all info format to Modify
+  validateAllInfoToModify() {
+    let message = '';
+    if (!this.validateNamesAndLastNamesToModify('name')) { message += '·Se debe registrar un solo nombre. <br>'; }
+    if (!this.validateNamesAndLastNamesToModify('fatherLastName')) { message += '·Se debe registrar un solo apellido paterno.<br>'; }
+    if (!this.validateNamesAndLastNamesToModify('motherLastName')) { message += '·Se debe registrar un solo apellido materno.<br>'; }
+    const rutResponse = this.checkRut(this.diccModifyDriverForm['rut']);
+    if (!rutResponse['result']) {
+      if (rutResponse['content'] === 'invalid digit') { message += '·Dígito verificador de rut erróneo.<br>';
+      } else { message += '·Extensión del rut errónea.<br>'; }
+    }
+    if (!this.diccModifyDriverForm['unionized']) { message += '·Falta condición de sindicato del conductor.<br>'; }
+    if (message !== '') { this.missingInfoAlert(message);
+    } else {
+      this.diccModifyDriverForm['name'] = this.convertNames(this.diccModifyDriverForm['name']);
+      this.diccModifyDriverForm['fatherLastName'] = this.convertNames(this.diccModifyDriverForm['fatherLastName']);
+      this.diccModifyDriverForm['motherLastName'] = this.convertNames(this.diccModifyDriverForm['motherLastName']);
+      this.diccModifyDriverForm['rut'] = this.setFormatToCorrectRut(this.checkRut(this.diccModifyDriverForm['rut'])['content']);
+      this.updateDriverInfoAlert();
+    }
+  }
+
+
+  // Update driver info alert.
+  async updateDriverInfoAlert() {
+    const alert = await this.alertController.create({
+      header: 'Actualizar Conductor',
+      subHeader: `Deseas actualizar la información del siguiente conductor:`,
+      message: `·Nombre: ${this.diccModifyDriverForm['name']}
+                           ${this.diccModifyDriverForm['fatherLastName']}
+                           ${this.diccModifyDriverForm['motherLastName']} <br>
+               ·Rut: ${this.diccModifyDriverForm['rut']}<br>
+                `,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            this.clearModifyForm();
+            this.driverToModify = null;
+            this.selectDriverToModifyDisable = false;
+          }
+        }, {
+          text: 'Aceptar',
+          handler: () => {
+            this.updateDriver(this.driverToModify['_id']);
+            this.selectDriverToModifyDisable = false;
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+
+  // Clear form.
+  clearModifyForm() {
+    this.diccModifyDriverForm['name'] = null;
+    this.diccModifyDriverForm['fatherLastName'] = null;
+    this.diccModifyDriverForm['motherLastName'] = null;
+    this.diccModifyDriverForm['rut'] = null;
+    this.diccModifyDriverForm['unionized'] = null;
+  }
+
+
+  // Update driver from backend.
+  updateDriver(driverId: string) {
+    const updateDriverObject = {};
+    updateDriverObject['name'] = this.diccModifyDriverForm['name'] + ' ' +
+                            this.diccModifyDriverForm['fatherLastName'] + ' ' +
+                            this.diccModifyDriverForm['motherLastName'];
+    updateDriverObject['rut'] = this.diccModifyDriverForm['rut'];
+    updateDriverObject['unionized'] = this.diccModifyDriverForm['unionized'] === 'No' ? false : true;
+    this.driversService.updateDriver(driverId, updateDriverObject).subscribe(
+      driverUpdated => {
+        this.clearModifyForm();
+        this.selectDriverToModifyDisable = false;
+        this.driverToModify = null;
+        this.succesfulRegistrationAlert();
+        console.log('Conductor actualizado: ', driverUpdated);
+      },
+      error => {
+        this.clearModifyForm();
+        this.selectDriverToModifyDisable = false;
+        this.driverToModify = null;
+        this.failedRegistrationAlert();
+        console.log('Error updating driver: ', error);
+      }
+    );
+  }
+
+
+  // Succesful registration alert.
+  async succesfulRegistrationAlert() {
+    const alert = await this.alertController.create({
+      header: 'Conductor Registrado',
+      subHeader: 'Conductor registrado exitosamente!',
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {}
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+
+  // Failed registration alert.
+  async failedRegistrationAlert() {
+    const alert = await this.alertController.create({
+      header: 'Error Registrando Conductor',
+      subHeader: 'Ha ocurrido un error en el registro. Inténtelo nuevamente.',
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {}
+        }
+      ]
+    });
+    await alert.present();
+  }
 
 }
