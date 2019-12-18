abf86174671d8e3c9512ff7efbb77a5b52c9073c
Enrique <eiurrutia@uc.cl>
2019-12-18 12:09:17 -0300

    importers page created

---
+++ src/app/importers/importers.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-importers',
+  templateUrl: './importers.page.html',
+  styleUrls: ['./importers.page.scss'],
+})
+export class ImportersPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
