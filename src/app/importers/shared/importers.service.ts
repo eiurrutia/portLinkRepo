f61b3a695b0cdf92a01fbdb3d95fcde0350c46c2
Enrique <eiurrutia@uc.cl>
2019-12-18 13:39:18 -0300

    main functions added

--- src/app/importers/shared/importers.service.ts
+++ src/app/importers/shared/importers.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
-import { Importer } from '../importer.model';
 import { Globals } from '../../globals';
 
 @Injectable({
@@ -23,6 +22,31 @@ export class ImportersService {
   getImporters(): Observable<any> {
     const url = `${this.backUrl}/importers/`;
     return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  getImporter(id: string): Observable<any> {
+    const url = `${this.backUrl}/importers/${id}/`;
+    return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  getImporterByName(name: string): Observable<any> {
+    const url = `${this.backUrl}/importers?name=${name}`;
+    return this.http.get<any>(url, this.httpOptions).catch(this.errorHandler);
+  }
+
+  createImporter(importer: any): Observable<any> {
+    const url = `${this.backUrl}/importers/`;
+    return this.http.post<any>(url, importer, this.httpOptions).catch(this.errorHandler);
+  }
+
+  updateImporter(importerId: string, importerObj: any): Observable<any> {
+    const url = `${this.backUrl}/importers/${importerId}`;
+    return this.http.patch<any>(url, importerObj, this.httpOptions).catch(this.errorHandler);
+  }
+
+  deleteImporter(importerId: string): Observable<any> {
+    const url = `${this.backUrl}/importers/${importerId}`;
+    return this.http.delete<any>(url, this.httpOptions).catch(this.errorHandler);
   }
 
   errorHandler(error: HttpErrorResponse) {
